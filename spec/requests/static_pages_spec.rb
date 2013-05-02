6ff09fc07a6d6da32f75e9e9078c5f1481b5bb46
Juan Carlos <jjuanchow@gmail.com>
2013-05-02 00:12:28 -0500

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+  	it "should have the content 'iTuits'" do
+  		visit '/static_pages/home'
+  		page.should have_content('iTuits')
+  	end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector 'title', :text => 'iTuits | Home'
+    end
+  end
+
+  describe "Help page" do
+  	it "should have the content 'Ayuda'" do
+  		visit '/static_pages/help'
+  		page.should have_content 'Ayuda'
+  	end
+
+    it "should have the title 'Ayuda'" do
+      visit '/static_pages/help'
+      page.should have_selector 'title', :text => 'iTuits | Ayuda'
+    end
+  end
+
+  describe "Acerca de" do
+    it "should have the content 'Acerca De'" do
+      visit '/static_pages/about'
+      page.should have_content 'Acerca De'
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector 'title', :text => 'iTuits | About'
+    end
+  end
+end
